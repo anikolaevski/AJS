8273c93a2f4f2f54faf55dc4ae3436f881482d39
anikolaevski <anikolaevski@gmail.com>
2019-05-11 20:39:12 +0300

    task#3 sort function

--- standarts/standards_03/src/js/app.js
+++ standarts/standards_03/src/js/app.js
@@ -1,12 +1,12 @@
-"use strict"
-console.log('Привет!');
 console.log('Task#3');
 function bestRating (arr) {
   return Math.max.apply(null,arr);
 };
 
 function avgTop3(arr) {
-  arr.sort().reverse();
+  arr.sort(
+    (a,b) => { return b - a; }
+  );
   let sum = 0;
   for (let i = 0; i <= 2; i++) {
     sum += arr[i];
